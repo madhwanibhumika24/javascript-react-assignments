8c34d9ce9e999e0f624c5e8fa52d0978e36a5117
Bhumika Madhwani <madhwanibhumika24@gmail.com>
2026-09-24 14:53:44 +0530

    Add calculator validation and styling

---
+++ 1. JavaScript Calculator using Functions/script.js
@@ -0,0 +1,36 @@
+function calculate() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+
+    let result;
+
+    switch (operator) {
+
+        case "+":
+            result = num1 + num2;
+            break;
+
+        case "-":
+            result = num1 - num2;
+            break;
+
+        case "*":
+            result = num1 * num2;
+            break;
+
+        case "/":
+            if (num2 === 0) {
+                result = "Cannot divide by zero";
+            } else {
+                result = num1 / num2;
+            }
+            break;
+
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
